988130a3e0a094c52f9086a693ac0f21592f34c4
Taha Khan <93130261+tahakhan2004@users.noreply.github.com>
2023-12-25 17:14:15 +0500

    Update page.js

--- src/app/scriptCom/page.js
+++ src/app/scriptCom/page.js
@@ -1,16 +1,16 @@
-"use client"
-import Script from 'next/script'
-import React from 'react'
+// "use client"
+// import Script from 'next/script'
+// import React from 'react'
 
-const Scriptt = () => {
-  return (
-    <>
-    <Script src='/location.js'
-    onLoad={()=>{console.log("file loaded");}}
-    />
-    <h1>get user location</h1>
-    </>
-  )
-}
+// const Scriptt = () => {
+//   return (
+//     <>
+//     <Script src='/location.js'
+//     onLoad={()=>{console.log("file loaded");}}
+//     />
+//     <h1>get user location</h1>
+//     </>
+//   )
+// }
 
-export default Scriptt
+// export default Scriptt
